cc94556f35523c786540f934caf972857d53d892
Joyle Silva <joyle.silva>
2026-01-25 13:08:33 -0300

    Projeto demonstração TLS e mTLS

--- scripts/05-CreateClientTruststore.sh
+++ scripts/05-CreateClientTruststore.sh
@@ -4,10 +4,19 @@
 rm -rf client-truststore.p12
 
 # Cria o truststore em formato JKS com o certificado da CA
-openssl pkcs12 -export \
- -nokeys \
- -in client.crt \
- -out client-truststore.p12 \
- -name "client" \
- -passout pass:changeit \
- -jdktrust anyExtendedKeyUsage
+#openssl pkcs12 -export \
+# -nokeys \
+# -in client.crt \
+# -out client-truststore.p12 \
+# -name "client" \
+# -passout pass:changeit \
+# -jdktrust anyExtendedKeyUsage
+
+#Cria o truststore em formato PKCS12 com o certificado da CA usando keytool
+keytool -importcert \
+  -file client.crt \
+  -alias client \
+  -keystore client-truststore.p12 \
+  -storetype PKCS12 \
+  -storepass changeit \
+  -noprompt
